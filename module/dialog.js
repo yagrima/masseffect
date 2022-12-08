d3b58367a63f9203e660f07a050a1d48a9235273
yagrima <avalia@gmx.net>
2022-12-08 22:11:56 +0100

    changed dialog windows button order

--- module/dialog.js
+++ module/dialog.js
@@ -8,13 +8,13 @@ export async function GetSkillCheckOptions() {
             title: game.i18n.format("masseffect.chat.generictest"),
             content: html,
             buttons: {
-                normal: {
-                    label: game.i18n.format("masseffect.chat.roll"),
-                    callback: html => resolve(_processSkillCheckOptions(html[0].querySelector("form")))
-                },
                 cancel: {
                     label: game.i18n.format("masseffect.chat.cancel"),
                     callback: html => resolve({cancelled: true})
+                },
+                normal: {
+                    label: game.i18n.format("masseffect.chat.roll"),
+                    callback: html => resolve(_processSkillCheckOptions(html[0].querySelector("form")))
                 }
             },
             default: "normal",
