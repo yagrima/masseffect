9dc4a809c5b5f8efc894ea4e911fe4b4dff89bc6
yagrima <avalia@gmx.net>
2022-12-14 18:47:32 +0100

    ini tracker looking for v10 errors

--- module/combat.js
+++ module/combat.js
@@ -1,41 +1,39 @@
 export default class meCombat extends Combat {
     _sortCombatants(a,b){
-        console.log("meCombat entered.");
+        console.log("meCombatSorting entered.");
         const iniA = Number.isNumeric(a.initiative) ? a.initiative : - 99;
         const iniB = Number.isNumeric(b.initiative) ? b.initiative : - 99;
-
-        let difference = iniA - iniB;
-        console.log("#1: "+a+", #2: "+b+", Resultat: "+difference);
+        console.log("meCombatSorting #1");
+        let difference = iniA - iniB; 
         if(difference != 0) return difference;
-
+        console.log("meCombatSorting #2");
         const luckA = Number.isNumeric(a.luck) ? a.luck : -99;
         const luckB = Number.isNumeric(b.luck) ? b.luck : -99;
-
+        console.log("meCombatSorting #3");
         let luckDifference = luckA - luckB;
         if(luckDifference != 0) return luckDifference;
-
+        console.log("meCombatSorting #4");
         return a.tokenId - b.tokenId;
     }
     _prepareCombatant(c,scene,players,settings={}){
-        console.log("meCombat entered.");
+        console.log("meCombatPrepCombatants entered.");
         let combatant = super._prepareCombatant(c,scene,players,settings);
         //wenn INI-Wert nicht einzigartig, addiere 0.1 und versuche erneut
         return combatant;
     }
     async startCombat(){
-        console.log("meCombat entered.");
+        console.log("meCombatStartCombat entered.");
         await this.setupTurns();
         return super.startCombat();
     }
 
     async rollInitiative(ids,{formula=null,updateTurn=true,messageOptions={}}={}){
-        console.log("meCombat entered.");
+        console.log("meCombatRollIni entered.");
         const template = "systems/masseffect/templates/chat-initiative.html";
         ids = typeof ids === "string" ? [ids] : ids;
         const currentId = this.combatant?.id;
         const updates = [];
         const messages = [];
-
         for(let [i,id] of ids.entries()){
             const combatant = this.combatants.get(id);
             if(!combatant?.isOwner) continue;
@@ -43,7 +41,7 @@ export default class meCombat extends Combat {
             await roll.evaluate({async: true});
             updates.push({_id: id, initiative: roll.total})
             let templateContext = {roll: roll};
-
+            
             let chatDataIni = {
                 user: game.user.id,
                 speaker: ChatMessage.getSpeaker(combatant.actor),
@@ -51,15 +49,16 @@ export default class meCombat extends Combat {
                 sound: CONFIG.sounds.dice,
                 content: await renderTemplate(template,templateContext)
             };
+            
             if(i>0) chatDataIni.sound = null;
             messages.push(chatDataIni);
-
             if(!updates.length) return this;
             await this.updateEmbeddedDocuments("Combatant",updates);
             if(updateTurn && currentId){
                 await this.update({turn: this.turns.findIndex(t => t.id === currentId)});
             }
             await ChatMessage.implementation.create(messages);
+            console.log("meCombatRollIni left.");
             return this;
         }
     }
--- module/combattracker.js
+++ module/combattracker.js
@@ -4,21 +4,25 @@ export default class meCombatTracker extends CombatTracker {
     get template() {
         return "systems/masseffect/templates/combattracker.html";
     }
-    _onConfigureCombatant(li) {        
+    _onConfigureCombatant(li) {
+        console.log("data before");
         const combatant = this.viewed.combatants.get(li.data('combatant-id'));
+        console.log("data after");
         new meCombatant(combatant, {
-            top: Math.min(li[0].offsetTop, window.innerHeight - 350),
+            top: Math.min(li[0].offsetTop, window.innerHeight - 450),
             left: window.innerWidth - 720,
             width: 400
           }).render(true);  
     }
 
     async getData(options) {
+        console.log("getdata before");
         const data = await super.getData(options);
         if (!data.hasCombat) return data;
         for (let [i, combatant] of data.combat.turns.entries()) {
           data.turns[i].combatType = combatant.getFlag("masseffect", "combatType")
         }
+        console.log("getdata after");
         return data;
       }
 }
