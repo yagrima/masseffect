34cc132a30a34581e5e0d730f791fb8097e72f98
yagrima <avalia@gmx.net>
2022-12-17 01:34:57 +0100

    ini update 2022_12_17

--- module/combat.js
+++ module/combat.js
@@ -18,68 +18,22 @@ export default class meCombat extends Combat {
         //wenn INI-Wert nicht einzigartig, addiere 0.1 und versuche erneut
         return combatant;
     }
+    async _onToggleDefeatedStatus(combatant) {
+        console.log("the skull thingy");
+        combatant.defeated = !combatant.defeated;
+        return super._onToggleDefeatedStatus(combatant);
+    }
+
     async startCombat(){
+        console.log("startCombat");
         await this.setupTurns();
         return super.startCombat();
     }
     async resetAll() {
         await super.resetAll();
         return this.update({round: 0});
-    }
-    async previousTurn() {
-        return this.previousRound();
-    }
-    async nextTurn(nTicks = 0) {
-        if (nTicks == 0) {
-            let p = new Promise((resolve, reject) => {
-                let dialog = new Dialog({
-                    title: "Ticks",
-                    content: "<input type='text' class='ticks' value='3'>",
-                    buttons: {
-                        ok: {
-                            label: "Ok",
-                            callback: html => {
-                                resolve(parseInt(html.find('.ticks')[0].value));
-                            }
-                        }
-                    }
-                });
-                dialog.render(true);
-            });
-            nTicks = await p;
-        }
-        let combatant = this.combatant;
-        let newInitiative = Math.round(combatant.initiative) + nTicks;
-        return this.setInitiative(combatant.id, newInitiative);
-    }
-    async setInitiative(id, value, first = false) {
-        value = Math.round(value);
-        if (value < 10000) {
-            if (!first) {
-                value = this.combatants.reduce((acc, c) => {
-                    return ((Math.round(c.initiative) == value) ? Math.max((c.initiative || 0) + 0.01, acc) : acc);
-                }, value);
-            } else {
-                value = this.combatants.reduce((acc, c) => {
-                    return ((Math.round(c.initiative) == value) ? Math.min((c.initiative || 0) - 0.01, acc) : acc);
-                }, value);
-            }
-        } else {
-            if (value !== 10000 && value !== 20000) {
-                return
-            }
-        } 
-        await this.combatants.get(id).update({initiative: value});
-        if (this.started) {await this.nextRound();}        
-    }
-    get combatant() {return this.turns[0];}
-    get currentTick() {
-        if (this.turns) {
-            return Math.round(parseFloat(this.turns[0]?.initiative));
-        } else {
-            return null;
-        }   
-    }
+    } 
+
     async rollInitiative(ids,{formula=null,updateTurn=true,messageOptions={}}={}){
         const template = "systems/masseffect/templates/chat-initiative.html";
         ids = typeof ids === "string" ? [ids] : ids;
@@ -94,14 +48,13 @@ export default class meCombat extends Combat {
             await roll.evaluate({async: true});
             updates.push({_id: id, initiative: roll.total});
             let diceResults = roll.terms[0].results; //Ergänzung um isWild je Element nötig
-            for (let a in diceResults){
-                diceResults[a].isWild = false;
-            }
-            let templateContext = {roll: roll,
+            for (let a in diceResults){diceResults[a].isWild = false;}
+            let templateContext = {
+                roll: roll,
                 formula: formula,
                 rollResults: diceResults,
-                total: roll.total};
-            
+                total: roll.total
+            };
             let chatDataIni = {
                 user: game.user.id,
                 speaker: ChatMessage.getSpeaker(combatant.actor),
@@ -120,25 +73,5 @@ export default class meCombat extends Combat {
             await ChatMessage.implementation.create(messages);
             return this;
         }
-    }
-
-    async nextRound() {//await super.nextRound();
-        if (!this.started) return;
-        let nextRound = this.round + 1;
-        const updateData = { round: nextRound, turn: 0 };
-        this.setupTurns();
-        const updateOptions = {direction: 1 };
-        Hooks.callAll("combatRound", this, updateData, updateOptions);
-        return this.update(updateData);
-    }
-
-    async previousRound() {
-        if (!this.started) return;
-        return;
-    }
-
-    _onUpdateEmbeddedDocuments(embeddedName, documents, result, options, userId) {
-        this.setupTurns();//otherwise the next player is not marked correctly
-        super._onUpdateEmbeddedDocuments(embeddedName, documents, result, options, userId); 
     }
 }
